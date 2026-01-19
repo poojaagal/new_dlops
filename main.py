5dc9c5ea631d08482d076e7839fa9062e446b9f4
poojaagal <poojaagal14.pa@gmail.com>
2026-01-19 20:12:00 +0530

    gitignore file added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.cnnClassifier import logger 
 
-logger.info("Welcome to my personal Log") 
+#logger.info("Welcome to my personal Log") 
+
+
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
